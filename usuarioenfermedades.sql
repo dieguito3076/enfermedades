52546dd934dc8bc0b24f4c3d13b4fdc60f8d75d6
Diego Arturo Velazquez Trejo <diegovelazqu@hotmail.com>
2018-07-26 01:06:28 -0500

    nueva database

--- usuarioenfermedades.sql
+++ usuarioenfermedades.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `enfermedades`
+--
+
+DROP TABLE IF EXISTS `enfermedades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enfermedades` (
+  `idEnfermedad` varchar(50) CHARACTER SET armscii8 COLLATE armscii8_bin NOT NULL,
+  `Foto` varchar(50) CHARACTER SET armscii8 COLLATE armscii8_bin NOT NULL,
+  `descripcion` varchar(150) CHARACTER SET armscii8 COLLATE armscii8_bin NOT NULL,
+  `medicamento` int(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idEnfermedad`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enfermedades`
+--
+
+LOCK TABLES `enfermedades` WRITE;
+/*!40000 ALTER TABLE `enfermedades` DISABLE KEYS */;
+INSERT INTO `enfermedades` VALUES ('Faringitis','Imagenes/faringitis.jpg','Inflamaci?n de la faringe que puede provocar dolor de garganta, sequedad, enrojecimiento de la mucosa y, en ocasiones, fiebre.',1234);
+/*!40000 ALTER TABLE `enfermedades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `lecturaspulso`
 --
 
@@ -88,6 +114,31 @@ INSERT INTO `lecturastemp` VALUES (1,34),(2,34.26),(3,34.28),(4,34.3),(5,34.3),(
 UNLOCK TABLES;
 
 --
+-- Table structure for table `medicamentos`
+--
+
+DROP TABLE IF EXISTS `medicamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medicamentos` (
+  `idMedicamento` varchar(4) COLLATE ascii_bin NOT NULL,
+  `Foto` varchar(50) CHARACTER SET armscii8 COLLATE armscii8_bin NOT NULL,
+  `descripcion` varchar(180) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idMedicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medicamentos`
+--
+
+LOCK TABLES `medicamentos` WRITE;
+/*!40000 ALTER TABLE `medicamentos` DISABLE KEYS */;
+INSERT INTO `medicamentos` VALUES ('1','FotosMedicamentos/pills.jpg','Penicilina, Las penicilinas son antibióticos del grupo de los betalact?micos empleados profusamente en el tratamiento de infecciones provocadas por bacterias sensibles.'),('2','FotosMedicamentos/pills1.jpg','Cefalexina es un antibiótico del grupo de las cefalosporinas de los conocidos como de primera generaci?n. Es utilizado para tratar infecciones bacterianas en el tracto respiratorio'),('3','FotosMedicamentos/pills2.jpg','La amoxicilina es un antibi?tico semisint?tico derivado de la penicilina. Se trata de una amino penicilina. Act?a contra un amplio espectro de bacterias, tanto Gram positivos como '),('4','FotosMedicamentos/pills.jpg','El pantoprazol es un medicamento inhibidor de la bomba de protones en el estómago usado en el tratamiento a corto plazo de las erosiones y úlceras causadas por enfermedades de refl');
+/*!40000 ALTER TABLE `medicamentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuarioe`
 --
 
@@ -96,9 +147,11 @@ DROP TABLE IF EXISTS `usuarioe`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuarioe` (
   `usuarioKey` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `cadenaVal` varchar(70) COLLATE ascii_bin DEFAULT '000000000000000',
+  `cadenaVal` char(80) COLLATE ascii_bin DEFAULT '000000000000000',
   `Edad` int(2) unsigned DEFAULT '0',
   `Peso` float unsigned DEFAULT '0',
+  `fotoCamara` varchar(20) COLLATE ascii_bin NOT NULL,
+  `diagnostico` varchar(60) COLLATE ascii_bin NOT NULL,
   PRIMARY KEY (`usuarioKey`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=ascii COLLATE=ascii_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -109,7 +162,7 @@ CREATE TABLE `usuarioe` (
 
 LOCK TABLES `usuarioe` WRITE;
 /*!40000 ALTER TABLE `usuarioe` DISABLE KEYS */;
-INSERT INTO `usuarioe` VALUES (1,'0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',0,0);
+INSERT INTO `usuarioe` VALUES (1,'1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,',0,0,'fotoCam/faringe.jpg','Faringitis');
 /*!40000 ALTER TABLE `usuarioe` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -122,4 +175,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-23  0:55:31
+-- Dump completed on 2018-07-26  1:05:44
